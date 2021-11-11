7228eb0e52a6b64b5046d0604553f168d34b5746
NullyG <86624542+NullyG@users.noreply.github.com>
2021-11-11 18:09:25 -0500

    updated portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,16 +8,25 @@ function closeForm() {
 }
 
 // This function displays the first image in the slideshow when the page loads
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// This function changes the slide when the left or right arrows are clicked
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+// This function changes the slide when the dots are clicked
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
 
 function showSlides(n) {
-    var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stoes them in the variable array "slides"
+    var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
     var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
     if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
-    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, to slideIndex is set to the length of the array "slides"
+    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
     }
